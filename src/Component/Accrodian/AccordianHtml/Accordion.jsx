13bab64a93e44f58d90b31427433516d1702d8a9
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-10 01:15:42 +0530

    🔥 Remo: initial unnecessary files

--- src/Component/Accrodian/AccordianHtml/Accordion.jsx
+++ src/Component/Accrodian/AccordianHtml/Accordion.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+
 
 function Accordion(props) {
+
+  // useState to handle text on click
+const [title, setTitle] = useState(false);
+
+function titleHandler(){
+  setTitle((title) => !title)
+}
+
   return (
-    <div className="item">
+    <div onClick={titleHandler} className={`item ${title ? 'open text' : ''}`}>
       <p className="number">
         {props.index < 9 ? `0${props.index + 1}` : props.index + 1}
       </p>
       <p className="text">{props.title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{props.text}</div>
+      <p className="icon">{title ? '-' : '+'}</p>
+
+      {title && <div className="content-box">{props.text}</div>}
+
     </div>
   );
 }
