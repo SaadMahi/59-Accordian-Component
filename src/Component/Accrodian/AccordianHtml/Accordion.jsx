e35a8093168b3f63d5a0bf74b2147f79a40e758b
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-10 20:16:23 +0530

    🚸 feat: improved user experience

--- src/Component/Accrodian/AccordianHtml/Accordion.jsx
+++ src/Component/Accrodian/AccordianHtml/Accordion.jsx
@@ -1,25 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 
 
 
 function Accordion(props) {
 
-  // useState to handle text on click
-const [title, setTitle] = useState(false);
+/*   // useState to handle text on click
+const [title, setTitle] = useState(false); */
+
+const isOpen = props.index === props.currOpen;
 
 function titleHandler(){
-  setTitle((title) => !title)
+  // setTitle((title) => !title)
+  props.setCurrOpen(isOpen ? null : props.index)
 }
 
   return (
-    <div onClick={titleHandler} className={`item ${title ? 'open text' : ''}`}>
+    <div onClick={titleHandler} className={`item ${isOpen ? 'open text' : ''}`}>
       <p className="number">
         {props.index < 9 ? `0${props.index + 1}` : props.index + 1}
       </p>
       <p className="text">{props.title}</p>
-      <p className="icon">{title ? '-' : '+'}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
 
-      {title && <div className="content-box">{props.text}</div>}
+      {isOpen && <div className="content-box">{props.text}</div>}
 
     </div>
   );
--- src/Component/Accrodian/AccordionItem.jsx
+++ src/Component/Accrodian/AccordionItem.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./AccordianHtml/Accordion";
 
 function AccordionItem(props) {
+
+  const [currOpen, setCurrOpen] = useState(null)
+
+
   return (
     <div className="accordion">
       {props.faqList.map((items, index) => (
         <Accordion
+          currOpen={currOpen}
+          setCurrOpen={setCurrOpen}
+
           key={index}
           title={items.title}
           text={items.text}
