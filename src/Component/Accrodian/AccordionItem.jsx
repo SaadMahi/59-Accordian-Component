5b1dab077f8522d740dfd4d08c596b77c011bfe8
Mahimkar Saad Tahzeeb <117567622+SaadMahi@users.noreply.github.com>
2023-10-10 22:49:35 +0530

    ♻️ refac: code formatted

--- src/Component/Accrodian/AccordionItem.jsx
+++ src/Component/Accrodian/AccordionItem.jsx
@@ -1,18 +1,15 @@
-import React, { useState } from "react";
-import Accordion from "./AccordianHtml/Accordion";
+import React, { useState } from 'react';
+import Accordion from './AccordianHtml/Accordion';
 
 function AccordionItem(props) {
-
-  const [currOpen, setCurrOpen] = useState(null)
-
+  const [currOpen, setCurrOpen] = useState(null);
 
   return (
-    <div className="accordion">
+    <div className='accordion'>
       {props.faqList.map((items, index) => (
         <Accordion
           currOpen={currOpen}
           setCurrOpen={setCurrOpen}
-
           key={index}
           title={items.title}
           text={items.text}
